36d164e7e2c97bc71546826b1675694c5fc4d987
pyrotech95 <Jamiepheywood@gmail.com>
2023-07-30 14:49:11 +0100

    Added verboseness and fixed the column section
    Also fixed an encoding issue

--- CSVtoTXT.py
+++ CSVtoTXT.py
@@ -29,7 +29,7 @@ for fileName in inputs:
     fileCount += 1
     try:
         filePath = options["inputFolder"] + fileName
-        columnFrame = pd.read_csv(filePath, usecols=[options["Column"]])
+        columnFrame = pd.read_csv(filePath, usecols=[options["Column"] - 1])
         
     except:
         print("Could not open file at: " + filePath)
@@ -40,7 +40,16 @@ for fileName in inputs:
         rowsToConvert = len(columnFrame)
     for i in range(rowsToConvert):
         data = columnFrame.iloc[[i]]
-        outputFile = open(options["outputFolder"] + "File" + str(fileCount) + "Row" +  str(i) + ".txt", "w")
+        outputFile = open(options["outputFolder"] + "File" + str(fileCount) + "Row" +  str(i) + ".txt", "w", encoding="utf-8")
         outputFile.write(data.to_string(header=False, index=False))
         outputFile.close()
+        if options["verbose"] & options["Rows"] >= 4:
+            if i == rowsToConvert:
+                print("100% complete")
+            elif i == int((rowsToConvert*0.75)):
+                print("75% complete")
+            elif i == int((rowsToConvert*0.5)):
+                print("50% complete")
+            elif i == int((rowsToConvert*0.25)):
+                print("25% complete")
 print ("Transfer Complete")
